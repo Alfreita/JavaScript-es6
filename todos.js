0bda7d048ecf524243f7e2941e6a41e3fc075728
Victor Almeida <victoralmeida@ibm.com>
2019-01-09 09:49:44 -0200

    criação do app TODO

--- todos.js
+++ todos.js
@@ -1,1 +1,19 @@
-alert('funcionou');
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var inputButton = document.querySelector('#app button');
+
+var todos=[
+    'Fazer cafe',
+    'Estudar javaScript',
+    'Acessar a comunidade da RocketSeat'
+];
+
+function renderTodos() {
+    for (todo of todos){
+        var todoElement = document.createAttribute('li');
+        var todoText = document.createTextNode(todo);
+
+        todoElement.appendChild(todoText);
+        
+    }
+}
