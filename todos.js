8bf04250e72207fafa800ed2174f66c5fdfd0811
Victor Almeida <victor.freitas08@hotmail.com>
2019-01-10 09:46:01 -0200

    renderizando dados

--- todos.js
+++ todos.js
@@ -2,7 +2,7 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var inputButton = document.querySelector('#app button');
 
-var todos=[
+var todos = [
     'Fazer cafe',
     'Estudar javaScript',
     'Acessar a comunidade da RocketSeat'
@@ -10,10 +10,11 @@ var todos=[
 
 function renderTodos() {
     for (todo of todos){
-        var todoElement = document.createAttribute('li');
+        var todoElement = document.createElement('li');
         var todoText = document.createTextNode(todo);
-
         todoElement.appendChild(todoText);
-        
+        listElement.appendChild(todoElement);
     }
 }
+
+renderTodos();
