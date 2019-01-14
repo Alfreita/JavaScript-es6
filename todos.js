f46538c703274656bd36ca8229013234015eccfc
Victor Almeida <victor.freitas08@hotmail.com>
2019-01-14 13:14:08 -0200

    local storage

--- todos.js
+++ todos.js
@@ -2,11 +2,8 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-var todos = [
-    'Fazer cafe',
-    'Estudar javaScript',
-    'Acessar a comunidade da RocketSeat'
-];
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
+  
 
 function renderTodos() {    
     listElement.innerHTML = '';
@@ -39,13 +36,18 @@ function addTodo(){
     todos.push(textTodo);
     inputElement.value = '';
     renderTodos();
+    saveToStorage();
 }
 
 buttonElement.onclick = addTodo;
-
 
 function deleteTodo(pos){
     todos.splice(pos,1);
     inputElement.value = '';
     renderTodos();
+    saveToStorage();
+}
+
+function saveToStorage(){
+    localStorage.setItem('list_todos',JSON.stringify(todos));
 }
