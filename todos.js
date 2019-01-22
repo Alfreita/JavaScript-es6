90aa99f571ebb0ffab54d12874c2db88ce1f2d33
Victor Almeida <victor.freitas08@hotmail.com>
2019-01-22 11:28:32 -0200

    requisição ajax

--- todos.js
+++ todos.js
@@ -1,53 +1,10 @@
-var listElement = document.querySelector('#app ul');
-var inputElement = document.querySelector('#app input');
-var buttonElement = document.querySelector('#app button');
+var xhr =  new XMLHttpRequest();
 
-var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
-  
+xhr.open('GET','https://api.github.com/users/Alfreita');
+xhr.send(null);
 
-function renderTodos() {    
-    listElement.innerHTML = '';
-
-    for (todo of todos){
-        var todoElement = document.createElement('li');
-        var todoText = document.createTextNode(todo);
-
-        var linkElement = document.createElement('a');
-
-        linkElement.setAttribute('href','#');
-
-        var pos = todos.indexOf(todo);
-        linkElement.setAttribute('onclick','deleteTodo('+pos+')');
-
-        var linkText = document.createTextNode('Excluir');
-
-        linkElement.appendChild(linkText);
-
-        todoElement.appendChild(todoText);
-        todoElement.appendChild(linkElement);
-        listElement.appendChild(todoElement);
+xhr.onreadystatechange = function(){
+    if (xhr.readyState ===4){
+        console.log(JSON.parse(xhr.responseText));
     }
-}
-
-renderTodos();
-
-function addTodo(){
-    var textTodo = inputElement.value;
-    todos.push(textTodo);
-    inputElement.value = '';
-    renderTodos();
-    saveToStorage();
-}
-
-buttonElement.onclick = addTodo;
-
-function deleteTodo(pos){
-    todos.splice(pos,1);
-    inputElement.value = '';
-    renderTodos();
-    saveToStorage();
-}
-
-function saveToStorage(){
-    localStorage.setItem('list_todos',JSON.stringify(todos));
 }
