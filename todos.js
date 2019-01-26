76eb47bbfcf0c0f3be3d1f5213577d5e55b1c95c
Victor Almeida <victor.freitas08@hotmail.com>
2019-01-26 11:33:15 -0200

    axios api

--- todos.js
+++ todos.js
@@ -1,24 +1,5 @@
-var minhaPrimeiraPromisse = function(){
-    return new Promise(function(resolve,reject){
 
-        var xhr =  new XMLHttpRequest();
-
-        xhr.open('GET','https://api.github.com/users/Alfreita');
-        xhr.send(null);
-
-        xhr.onreadystatechange = function(){
-        if (xhr.readyState === 4){
-                if( xhr.status === 200){
-                    resolve(JSON.parse(xhr.responseText));
-                }else {
-                    reject('Erro na requisição');
-                }
-            }
-        }
-    });
-}
-
-minhaPrimeiraPromisse()
+axios.get('https://api.github.com/users/Alfreita')
     .then(function(response){
         console.log(response);
     })
