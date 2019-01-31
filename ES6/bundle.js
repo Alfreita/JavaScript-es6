c04e097821739b4c5bd22c859d67b0b89c0a4f82
Victor Almeida <victor.freitas08@hotmail.com>
2019-01-31 13:52:38 -0200

    Desestruturação de codigo

--- ES6/bundle.js
+++ ES6/bundle.js
@@ -1,57 +1,22 @@
 "use strict";
 
-//*Aqui é como percorremos o array*//
-var arr = [1, 2, 3, 4, 5, 6];
-var newArr = arr.map(function (item, index) {
-  return item + index;
-});
-console.log(newArr); //*Aqui faz a soma dos valores do array
-
-var sum = arr.reduce(function (total, next) {
-  return total + next;
-});
-console.log(sum); //*Aqui faz o filtro do array
-
-var filter = arr.filter(function (item) {
-  return item % 2 === 0;
-});
-console.log(filter); //*Aqui faz a busca por um item do array
-
-var find = arr.find(function (item) {
-  return item === 4;
-});
-console.log(find);
-/************************************************************************** 
- * 
- * As mesmas funções acima so que com arrow function
- * 
-***************************************************************************/
-
-var newArr1 = arr.map(function (item, index) {
-  return item + index;
-});
-console.log(newArr1);
-var sum1 = arr.reduce(function (total, next) {
-  return total + next;
-});
-console.log(sum1);
-var filter1 = arr.filter(function (item) {
-  return item % 2 === 0;
-});
-console.log(filter1);
-var find1 = arr.find(function (item) {
-  return item === 4;
-});
-console.log(find1);
-var mult = arr.map(function (item) {
-  return item * 2;
-});
-console.log(mult);
-
-var teste = function teste() {
-  return {
-    nome: "victor"
-  };
+var usuario = {
+  nome: 'victor',
+  idade: 22,
+  endereco: {
+    cidade: 'campinas',
+    estado: 'SP'
+  }
 };
+var nome = usuario.nome,
+    idade = usuario.idade,
+    cidade = usuario.endereco.cidade;
+console.log(nome, idade, cidade);
 
-console.log(teste);
+function mostraNome(_ref) {
+  var nome = _ref.nome,
+      idade = _ref.idade;
+  console.log(nome, idade);
+}
+
+mostraNome(usuario);
--- ES6/main.js
+++ ES6/main.js
@@ -1,60 +1,17 @@
-//*Aqui é como percorremos o array*//
-const arr=[1,2,3,4,5,6];
+const usuario = {
+    nome: 'victor',
+    idade: 22,
+    endereco :{
+        cidade: 'campinas',
+        estado: 'SP',
+    },
+};
 
-const newArr = arr.map(function(item, index){
-    return item + index;
-});
-console.log(newArr);
+const {nome,idade,endereco:{cidade}} = usuario;
+console.log(nome,idade,cidade);
 
-//*Aqui faz a soma dos valores do array
-const sum = arr.reduce(function(total,next){
-    return total+next;
-});
-console.log(sum);
+function mostraNome({nome,idade}) {
+    console.log(nome,idade);   
+}
 
-//*Aqui faz o filtro do array
-const filter = arr.filter(function(item){
-    return item %  2 === 0;
-});
-console.log(filter);
-
-//*Aqui faz a busca por um item do array
-const find = arr.find(function(item){
-    return item === 4;
-});
-console.log(find);
-
-/************************************************************************** 
- * 
- * As mesmas funções acima so que com arrow function
- * 
-***************************************************************************/
-
-const newArr1 = arr.map((item, index) => {
-    return item + index;
-});
-console.log(newArr1);
-
-const sum1 = arr.reduce((total,next) =>{
-    return total+next;
-});
-console.log(sum1);
-
-const filter1 = arr.filter(item =>{
-    return item %  2 === 0;
-});
-console.log(filter1);
-
-const find1 = arr.find(item =>{
-    return item === 4;
-});
-console.log(find1);
-
-const mult = arr.map(item =>{
-    return item * 2;
-});
-console.log(mult);
-
-const teste = () => ({nome:"victor"});
-
-console.log(teste);
+mostraNome(usuario);
