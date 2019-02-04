dfd343fcbed0b5e9a8f29c211658050cfe9349a9
Victor Almeida <victor.freitas08@hotmail.com>
2019-02-04 14:03:41 -0200

    async e await

--- ES6/src/main.js
+++ ES6/src/main.js
@@ -1,1 +1,19 @@
-alert('teste');
+
+ const minhaPromisse = () => new Promise((resolve,reject) => {
+    setTimeout(() => {resolve('ok')},2000);
+ })
+ 
+ 
+ // async function executaPromisse() {
+ //    console.log(await minhaPromisse());
+ //    console.log(await minhaPromisse());
+ //    console.log(await minhaPromisse());
+ //}
+
+ const  executaPromisse = async () => {
+    console.log(await minhaPromisse());
+    console.log(await minhaPromisse());
+    console.log(await minhaPromisse());
+}
+
+ executaPromisse();
