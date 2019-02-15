6516ac54205a181e51ba0294051500fcc83d9caa
Victor Almeida <victor.freitas08@hotmail.com>
2019-02-15 11:38:47 -0200

    Render em tela

--- ES6/src/main.js
+++ ES6/src/main.js
@@ -1,7 +1,8 @@
 class App{
   constructor(){
-    this.repositories =[];
+    this.repositories = [];
     this.formEl = document.getElementById('repo-form');
+    this.listEl = document.getElementById('repo-list');
     this.registerHandler();
   }
 
@@ -9,18 +10,43 @@ class App{
     this.formEl.onsubmit = event => this.addRepository(event);
   }
 
-  
+
   addRepository(event){
     event.preventDefault();
 
     this.repositories.push({
       name:'RocketSeat',
       description:'Tire a sua ideia do papel e dê vida à sua startUp.',
-      avatar:'https://avatars0.githubusercontent.com/u/28929274?v=4',
-      html_url:'http:/github.com/rocketseat/rocketseat.com.br',
+      avatar_url:'https://avatars0.githubusercontent.com/u/28929274?v=4',
+      html_url:'https://github.com/rocketseat/rocketseat.com.br',
     });
 
-    console.log(this.repositories);
+    this.render();
+  }
+  render(){
+    this.listEl.innerHTML='';
+    this.repositories.forEach(repo =>{
+      let imgEl = document.createElement('img');  
+      imgEl.setAttribute('src',repo.avatar_url);
+
+      let titleEl = document.createElement('strong');
+      titleEl.appendChild(document.createTextNode(repo.name));
+
+      let descriptionEl = document.createElement('p')
+      descriptionEl.appendChild(document.createTextNode(repo.description));
+
+      let linkEl = document.createElement('a');
+      linkEl.setAttribute('target', '_blank');
+      linkEl.appendChild(document.createTextNode('Acessar'));
+
+      let listItemEl = document.createElement('li');
+      listItemEl.appendChild(imgEl);
+      listItemEl.appendChild(titleEl);
+      listItemEl.appendChild(descriptionEl);
+      listItemEl.appendChild(linkEl);
+
+     this.listEl.appendChild(listItemEl);  
+    }); 
   }
 }
 
