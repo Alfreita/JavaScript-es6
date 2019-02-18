4beed812a27493501722c44d6796827ae2600ed0
Victor Almeida <victor.freitas08@hotmail.com>
2019-02-18 10:52:28 -0300

    terminado

--- ES6/src/main.js
+++ ES6/src/main.js
@@ -1,8 +1,10 @@
+import api from './api';
 class App{
   constructor(){
     this.repositories = [];
     this.formEl = document.getElementById('repo-form');
     this.listEl = document.getElementById('repo-list');
+    this.inputEl = document.getElementById('input');
     this.registerHandler();
   }
 
@@ -10,18 +12,43 @@ class App{
     this.formEl.onsubmit = event => this.addRepository(event);
   }
 
+  setLoading(loading = true){
+    if(loading === true){
+      let loadingEl = document.createElement('span');
+      loadingEl.appendChild(document.createTextNode('Carregando'));
+      loadingEl.setAttribute('id','loading');
+      this.formEl.appendChild(loadingEl);
+    }else{
+      document.getElementById('loading').remove();
+    }
+  }
 
-  addRepository(event){
+   async  addRepository(event){
     event.preventDefault();
 
-    this.repositories.push({
-      name:'RocketSeat',
-      description:'Tire a sua ideia do papel e dê vida à sua startUp.',
-      avatar_url:'https://avatars0.githubusercontent.com/u/28929274?v=4',
-      html_url:'https://github.com/rocketseat/rocketseat.com.br',
-    });
+    const repoInput = this.inputEl.value;
 
+    if(repoInput.lenght === 0) return ;
+
+    this.setLoading();
+
+    try {
+      const response = await api.get(`/repos/${repoInput}`);
+
+    const {name,description,html_url,owner:{avatar_url}} = response.data;
+
+    this.repositories.push({
+      name,
+      description,
+      avatar_url,
+      html_url,
+    });
+    this.inputEl.value = ''; 
     this.render();
+    } catch (error) {
+      alert(`não foi possivel recuperar esse repositorio ${error}`);
+    }
+    this.setLoading(false);
   }
   render(){
     this.listEl.innerHTML='';
@@ -37,6 +64,7 @@ class App{
 
       let linkEl = document.createElement('a');
       linkEl.setAttribute('target', '_blank');
+      linkEl.setAttribute('href',repo.html_url);
       linkEl.appendChild(document.createTextNode('Acessar'));
 
       let listItemEl = document.createElement('li');
